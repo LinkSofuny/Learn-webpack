98ebe2518c4c8b7d46bfa1f6fd38287e62f4308a
陈渝东 <chenyud@shanhs.com>
2021-07-31 11:19:21 +0800

    完成管理资源学习

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from "./img.png"
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div')
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.innerHTML = _.join(['Hello', 'webpack', '&#xe621;'], ' ')
+    element.classList.add('hello')
+    element.classList.add('iconfont')
+
+    const myIcon = new Image()
+    myIcon.src = Icon
+    
+    element.appendChild(myIcon)
+
+    console.log(Data);
+    console.log(Notes);
 
     return element
 }
