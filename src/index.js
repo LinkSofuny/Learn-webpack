db2033a655dfb05d72929fdf7d04c013884ce9fe
陈渝东 <chenyud@shanhs.com>
2021-08-02 15:12:06 +0800

    完成开发环境, 管理输出学习

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash'
-
-console.log(json.owner.name); // output `Tom Preston-Werner`
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div')
+    const btn = document.createElement('button')
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
     // ..
+    btn.innerHTML = 'Click me and check the console!'
+
+    btn.onclick = printMe
+
+    element.appendChild(btn)
 
     return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require('path')
-
+const HtmlWebpackPulgin = require('html-webpack-plugin')
 
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPulgin({
+            title: 'Development'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/'
+    },
+    
 
 }
