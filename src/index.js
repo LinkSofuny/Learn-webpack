9a0b252155fcfcd0eaa644d40a60e06452851b41
Link <cyd551646@sina.com>
2021-08-05 13:54:58 +0800

    动态导入, 懒加载

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+import _ from 'lodash'
 
-async function getComponent() {
+function component() {
 
-    const { default: _ } = await import('lodash')
     const element = document.createElement('div')
+    const button = document.createElement('button')
+    const br = document.createElement('br')
+
 
     element.innerHTML = _.join(['hello', 'webpack'], '')
+    button.innerHTML = 'Click me and look at the console!'
+    element.appendChild(br)
+    element.appendChild(button)
 
+    button.onclick= e => import( /* webpackChunkName: 'print' */ './print' ).then( module => {
+        const print = module.default
+
+        print()
+    })
+    
     return element
     
 }
-
-getComponent().then( component => document.body.appendChild(component) )
+document.body.appendChild(component());
