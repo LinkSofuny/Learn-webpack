aabc259b7cf1bac1858939da1dd8e0d3519afe1e
Link <cyd551646@sina.com>
2021-08-06 14:01:02 +0800

    完成缓存

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import _ from 'lodash'
-
+// import Print from './print'
 function component() {
 
     const element = document.createElement('div')
-    const button = document.createElement('button')
-    const br = document.createElement('br')
-
 
     element.innerHTML = _.join(['hello', 'webpack'], '')
-    button.innerHTML = 'Click me and look at the console!'
-    element.appendChild(br)
-    element.appendChild(button)
-
-    button.onclick= e => import( /* webpackChunkName: 'print' */ './print' ).then( module => {
-        const print = module.default
-
-        print()
-    })
+    // element.onclick = Print.bind(null, 'Hello webpack!');
     
     return element
     
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPulgin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
+        main: './src/index.js',
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -13,14 +13,27 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPulgin({
-            title: 'Development'
+            title: 'Caching'
         })
     ],
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         publicPath: '/'
     },
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
 
 }
